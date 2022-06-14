51bfa15a75dcb81577b5d8593e18487c6c527262
Lukasz Kipka <lukasz.kipka@pb.com>
2022-06-14 15:02:45 +0200

    Reverse by Max

--- src/app/reverse.pipe.ts
+++ src/app/reverse.pipe.ts
@@ -6,11 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReversePipe implements PipeTransform {
 
   transform(value: string): string {
-    if (value.length === 0) {
-      return value;
-    }
-    const stringChars: string[] = value.split('');
-    stringChars.reverse();
-    return stringChars.join('');
+    // if (value.length === 0) {
+    //   return value;
+    // }
+    // const stringChars: string[] = value.split('');
+    // stringChars.reverse();
+    // return stringChars.join('');
+
+    return value.split('').reverse().join('');
   }
 }
